1ae6677450ee80e4388a1ee68475d322f0ca81db
Casey K <tehcnicolour@gmail.com>
2020-03-27 12:52:07 -0700

    static form

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
 // graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
-const wrapper = promise =>
-  promise.then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-    return result
-  })
+// const wrapper = promise =>
+//   promise.then(result => {
+//     if (result.errors) {
+//       throw result.errors
+//     }
+//     return result
+//   })
 
 // exports.createPages = async ({ graphql, actions }) => {
 //   const { createPage } = actions
